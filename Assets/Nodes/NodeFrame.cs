b457acf64c209c5a02f75fab10b7b464bfc3e006
local9 <apjcookie1@hotmail.com>
2023-08-30 01:08:49 +0100

    refactor: use a list of strings

--- Assets/Nodes/NodeFrame.cs
+++ Assets/Nodes/NodeFrame.cs
@@ -6,29 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class NodeFrame : MonoBehaviour
 {
-    [SerializeField] public TextMeshPro NameTextComponent;
-    [SerializeField] public TextMeshPro TimerTextComponent;
-    [SerializeField] public TextMeshPro ScanTextComponent;
-    [SerializeField] public string Name;
-    [SerializeField] public DateTime Time;
+    [SerializeField] public TextMeshPro TmpNameComponent;
+    [SerializeField] public TextMeshPro TmpTimerComponent;
+    [SerializeField] public TextMeshPro TmpScanComponent;
+    [SerializeField] public string NodeName;
+    [SerializeField] public DateTime NodeTime;
     private GameManager gameManager;
+
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        UpdateUIText(Name);
+        UpdateUIText(NodeName);
     }
+
+    /// <summary>
+    /// Update the UI text for the node
+    /// </summary>
     void Update()
     {
-        float nodeTime = Convert.ToSingle(Math.Round((DateTime.Now - Time).TotalMinutes));
-        if (nodeTime < 60) TimerTextComponent.text = Mathf.Round(nodeTime) + "m";
-        else if (nodeTime < 1440) TimerTextComponent.text = Mathf.Round(nodeTime / 60) + "h";
-        else TimerTextComponent.text = Mathf.Round(nodeTime / 1440) + "d";
+        int minutesSinceScan = Convert.ToInt32(Math.Round((DateTime.Now - NodeTime).TotalMinutes));
+        if (minutesSinceScan < 60) TmpTimerComponent.text = Mathf.Round(minutesSinceScan) + "m";
+        else if (minutesSinceScan < 1440) TmpTimerComponent.text = Mathf.Round(minutesSinceScan / 60) + "h";
+        else TmpTimerComponent.text = Mathf.Round(minutesSinceScan / 1440) + "d";
     }
+
+    /// <summary>
+    /// Remove sigs from the node's sig list
+    /// </summary>
     public void RemoveSigs()
     {
         foreach (Node node in gameManager.WorkingRoute.NodeList)
         {
-            if (node.Name == Name)
+            if (node.Name == NodeName)
             {
                 node.SigList.Clear();
                 node.Update = DateTime.Now;
@@ -38,23 +47,28 @@ public class NodeFrame : MonoBehaviour
             }
         }
     }
+
+    /// <summary>
+    /// Import sigs from clipboard and update the node's sig list
+    /// </summary>
     public void UpdateSigs()
     {
         Debug.Log("updating sigs");
-        // import sigs here and apply to list
-        var NewStrings = new List<string>(GUIUtility.systemCopyBuffer.Split('\n'));
+        List<string> NewStrings = new List<string>(GUIUtility.systemCopyBuffer.Split('\n'));
+
         if (NewStrings == null) return;
         if (NewStrings.Count == 0) return;
         if (NewStrings[0][3] != '-') return;
+
         foreach (Node node in GameObject.Find("GameManager").GetComponent<GameManager>().WorkingRoute.NodeList)
         {
-            if (node.Name == Name)
+            if (node.Name == NodeName)
             {
                 Debug.Log("node name match");
                 // Compare old sigs to new, update old list
-                for (var i = 0; i < NewStrings.Count; i++)
+                for (int i = 0; i < NewStrings.Count; i++)
                 {
-                    for (var j = 0; j < node.SigList.Count; j++)
+                    for (int j = 0; j < node.SigList.Count; j++)
                     {
                         if (NewStrings[i].Substring(0, 7) == node.SigList[j].Substring(0, 7))
                         {
@@ -67,91 +81,79 @@ public class NodeFrame : MonoBehaviour
                         }
                     }
                 }
+
                 Debug.Log("updated sigs");
                 node.SigList = NewStrings;
                 node.Update = DateTime.Now;
+
                 gameManager.WorkingRoute.CameraY = transform.position.y;
-                var camPos = Camera.main.transform.position;
+                Vector3 camPos = Camera.main.transform.position;
                 Camera.main.transform.position = new Vector3(camPos.x, transform.position.y, camPos.z);
                 gameManager.ClampCamera();
+
                 Serializer.SaveData(gameManager.WorkingRoute);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                 return;
             }
         }
     }
+
+    /// <summary>
+    /// Update the UI text for the node based on the node's sig list
+    /// </summary>
+    /// <param name="nodeName"></param>
     void UpdateUIText(string nodeName)
     {
-        List<string> strings = new List<string>();
-        var nodeList = GameObject.Find("GameManager").GetComponent<GameManager>().WorkingRoute.NodeList;
+        List<string> lstSignatures = new List<string>();
+        List<Node> nodeList = GameObject.Find("GameManager").GetComponent<GameManager>().WorkingRoute.NodeList;
+
         foreach (Node node in nodeList)
         {
             if (nodeName == node.Name)
             {
-                strings = node.SigList;
+                lstSignatures = node.SigList;
             }
         }
 
-        NameTextComponent.text = Name;
+        TmpNameComponent.text = NodeName;
 
         // Convert list of raw sigs to human-readable format
-        var total = 0;
-        var solved = 0;
-        var unsolved = 0;
-        var targets = 0;
-        ScanTextComponent.text = "";
-        var tempString = "";
-        foreach (string sig in strings)
+        int total = lstSignatures.Count;
+        int solved = 0;
+        int unsolved = 0;
+        TmpScanComponent.text = "";
+        List<string> tempString = new List<string>(); // change to a list of strings
+
+        foreach (string sig in lstSignatures)
         {
-            /*displayString += "• ";
-            displayString += sig.Substring(0, 3) + " ";
-            if (sig.Contains("Covert Research")) displayString += "→ GHOST";
-            else if (sig.Contains("Sleeper Cache")) displayString += "→ SLEEPER";
-            else if (sig.Contains("Nebula")) displayString += "→ NEBULA";
-            else if (sig.Contains("Data Site")) displayString += "";
-            else if (sig.Contains("Relic Site")) displayString += "";
-            else if (sig.Contains("Wormhole")) displayString += "";
-            else if (sig.Contains("Combat Site")) displayString += "";
-            else displayString += "— ???";
-            displayString += "\n";*/
-            total++;
             if (sig.Contains("Covert Research"))
             {
-                tempString += sig.Substring(0, 3) + " [GS], ";
-                targets++; solved++;
+                tempString.Add($"{sig.Substring(0, 3)} [GS]");
+                solved++;
             }
             else if (sig.Contains("Sleeper Cache"))
             {
-                tempString += sig.Substring(0, 3) + " [SC], ";
-                targets++; solved++;
+                tempString.Add($"{sig.Substring(0, 3)} [SC]");
+                solved++;
             }
             else if (sig.Contains("Nebula"))
             {
-                //{tempString+= sig.Substring(0, 3) + " [nebu], ";
-                if (sig.Contains("Sister")) { tempString += sig.Substring(0, 3) + " [LiM20], "; targets++; }
-                else if (sig.Contains("Helix")) { tempString += sig.Substring(0, 3) + " [LiM60], "; targets++; }
-                else if (sig.Contains("Wild")) { tempString += sig.Substring(0, 3) + " [MaM20], "; targets++; }
-                else if (sig.Contains("Blackeye")) { tempString += sig.Substring(0, 3) + " [MaM60], "; targets++; }
-                else if (sig.Contains("Sunspark")) { tempString += sig.Substring(0, 3) + " [AmM20], "; targets++; }
-                else if (sig.Contains("Diablo")) { tempString += sig.Substring(0, 3) + " [AmM60], "; targets++; }
-                else if (sig.Contains("Smoking")) { tempString += sig.Substring(0, 3) + " [GoM20], "; targets++; }
-                else if (sig.Contains("Ring")) { tempString += sig.Substring(0, 3) + " [GoM60], "; targets++; }
-                else if (sig.Contains("Calabash")) { tempString += sig.Substring(0, 3) + " [CeM20], "; targets++; }
-                else if (sig.Contains("Glass")) { tempString += sig.Substring(0, 3) + " [CeM60], "; targets++; }
-                else if (sig.Contains("Bright")) { tempString += sig.Substring(0, 3) + " [ViM20], "; targets++; }
-                else if (sig.Contains("Sparking")) { tempString += sig.Substring(0, 3) + " [ViM60], "; targets++; }
-                else if (sig.Contains("Ghost")) { tempString += sig.Substring(0, 3) + " [AzM20], "; targets++; }
-                else if (sig.Contains("Eagle")) { tempString += sig.Substring(0, 3) + " [AzM60], "; targets++; }
-                else if (sig.Contains("Flame")) { tempString += sig.Substring(0, 3) + " [VeM20], "; targets++; }
-                else if (sig.Contains("Pipe")) { tempString += sig.Substring(0, 3) + " [VeM60], "; targets++; }
-                /*else if (sig.Contains("Emerald")) {tempString+= sig.Substring(0, 3) + " [LiC5], ";}
-                else if (sig.Contains("Crimson")) {tempString+= sig.Substring(0, 3) + " [MaC5], ";}
-                else if (sig.Contains("Bandit")) {tempString+= sig.Substring(0, 3) + " [AmC5], ";}
-                else if (sig.Contains("Profiteer")) {tempString+= sig.Substring(0, 3) + " [GoC5], ";}
-                else if (sig.Contains("Phoenix")) {tempString+= sig.Substring(0, 3) + " [CeC5], ";}
-                else if (sig.Contains("Forgotten")) {tempString+= sig.Substring(0, 3) + " [ViC5], ";}
-                else if (sig.Contains("Rapture")) {tempString+= sig.Substring(0, 3) + " [AzC5], ";}
-                else if (sig.Contains("Saintly")) {tempString+= sig.Substring(0, 3) + " [VeC5], ";}*/
+                if (sig.Contains("Sister")) { tempString.Add($"{sig.Substring(0, 3)} [LiM20]"); }
+                else if (sig.Contains("Helix")) { tempString.Add($"{sig.Substring(0, 3)} [LiM60]"); }
+                else if (sig.Contains("Wild")) { tempString.Add($"{sig.Substring(0, 3)} [MaM20]"); }
+                else if (sig.Contains("Blackeye")) { tempString.Add($"{sig.Substring(0, 3)} [MaM60]"); }
+                else if (sig.Contains("Sunspark")) { tempString.Add($"{sig.Substring(0, 3)} [AmM20]"); }
+                else if (sig.Contains("Diablo")) { tempString.Add($"{sig.Substring(0, 3)} [AmM60]"); }
+                else if (sig.Contains("Smoking")) { tempString.Add($"{sig.Substring(0, 3)} [GoM20]"); }
+                else if (sig.Contains("Ring")) { tempString.Add($"{sig.Substring(0, 3)} [GoM60]"); }
+                else if (sig.Contains("Calabash")) { tempString.Add($"{sig.Substring(0, 3)} [CeM20]"); }
+                else if (sig.Contains("Glass")) { tempString.Add($"{sig.Substring(0, 3)} [CeM60]"); }
+                else if (sig.Contains("Bright")) { tempString.Add($"{sig.Substring(0, 3)} [ViM20]"); }
+                else if (sig.Contains("Sparking")) { tempString.Add($"{sig.Substring(0, 3)} [ViM60]"); }
+                else if (sig.Contains("Ghost")) { tempString.Add($"{sig.Substring(0, 3)} [AzM20]"); }
+                else if (sig.Contains("Eagle")) { tempString.Add($"{sig.Substring(0, 3)} [AzM60]"); }
+                else if (sig.Contains("Flame")) { tempString.Add($"{sig.Substring(0, 3)} [VeM20]"); }
+                else if (sig.Contains("Pipe")) { tempString.Add($"{sig.Substring(0, 3)} [VeM60]"); }
                 solved++;
             }
             else if (sig.Contains("Data Site")) solved++;
@@ -161,30 +163,24 @@ public class NodeFrame : MonoBehaviour
             else if (sig.Contains("Gas Site")) solved++;
             else
             {
-                tempString += sig.Substring(0, 3) + ", ";
+                tempString.Add(sig.Substring(0, 3));
                 unsolved++;
             }
         }
-        var filtered = solved - targets;
+
+        int filtered = solved - tempString.Count;
         if (filtered > 0)
         {
-            ScanTextComponent.text += "" + filtered + " ";
-            if ((unsolved > 0) || (targets > 0))
+            TmpScanComponent.text += "" + filtered + " ";
+            if ((unsolved > 0) || (tempString.Count > 0))
             {
-                ScanTextComponent.text += "+ ";
+                TmpScanComponent.text += "+ ";
             }
         }
-        // clean up the last space and comma
-        if (tempString.Length > 0)
-        {
-            ScanTextComponent.text += tempString.Substring(0, tempString.Length - 2);
-        }
-        //SigTextComponent.text = " (" + solved + "/" + total + ")";
-        if (targets > 0) ScanTextComponent.color = Color.green;
-        else ScanTextComponent.color = Color.white;
-        //else if (unsolved == 0) NameTextComponent.color = Color.green;
-        //else if (solved > 1) NameTextComponent.color = Color.yellow;
-        //else if ((DateTime.Now - Time).TotalMinutes < 60) TimerTextComponent.color = Color.Lerp(Color.white, Color.yellow, ((float)(DateTime.Now - Time).TotalMinutes) / 60);
-        //else if ((DateTime.Now - Time).TotalMinutes >= 60) TimerTextComponent.color = Color.Lerp(Color.yellow, Color.red, (((float)(DateTime.Now - Time).TotalMinutes) - 60) / 540);
+
+        if (tempString.Count > 0) TmpScanComponent.text = String.Join(", ", tempString);
+
+        if (tempString.Count > 0) TmpScanComponent.color = Color.green;
+        else TmpScanComponent.color = Color.white;
     }
 }
